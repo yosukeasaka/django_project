bfe16393231dc55cf2706fa9f55893606e31545a
yoshi <tankorian@gmail.com>
2019-08-07 18:02:58 +0900

    add change password function

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,3 +129,10 @@ CRISPY_TEMPLATE_PACH = 'bootstrap4'
 
 LOGIN_REDIRECT_URL ='blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('yousukeasaka@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('yoshiamerica')
